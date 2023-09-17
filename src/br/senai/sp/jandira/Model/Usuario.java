5bc95205345c9a02eb723d40783c49e7e64a7503
Gustavo Henrique <123705623+oghenrique@users.noreply.github.com>
2023-09-17 01:05:05 -0300

    usuario finalizado

--- src/br/senai/sp/jandira/Model/Usuario.java
+++ src/br/senai/sp/jandira/Model/Usuario.java
@@ -1,7 +1,31 @@
 package br.senai.sp.jandira.Model;
 
 public class Usuario {
-    String nome;
-    String email;
-    String endereco;
+    String nome, email, endereco, telefone;
+
+    public Usuario(String nome, String email, String endereco, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public Usuario() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
 }
