7a734daee21add04a6a0885e896bee9171825242
Gustavo Henrique <123705623+oghenrique@users.noreply.github.com>
2023-09-17 01:04:00 -0300

    printando

--- src/br/senai/sp/jandira/Model/Output.java
+++ src/br/senai/sp/jandira/Model/Output.java
@@ -1,24 +1,26 @@
 package br.senai.sp.jandira.Model;
 
+import java.util.List;
+
 public class Output {
     public void PrintUsuario(Usuario usuario) {
-        System.out.println("/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/");
-        System.out.println("--------------- INFORMAÇÕES DO LEITOR ---------------");
-        System.out.println("Nome do usuário: " + usuario.nome);
-        System.out.println("Email do usuário: " + usuario.email);
-        System.out.println("Endereço do usuário: " + usuario.endereco);
         System.out.println("---------------------------------------------------");
-        System.out.println("/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/");
+        System.out.println("                  INFORMAÇÕES DO LEITOR                ");
+        System.out.println("Nome do leitor: " + usuario.getNome());
+        System.out.println("Email do leitor: " + usuario.getEmail());
+        System.out.println("Endereço do leitor: " + usuario.getEndereco());
+        System.out.println("Telefone do leitor: " + usuario.getTelefone());
+        System.out.println("---------------------------------------------------");
     }
 
     public void PrintLivro(Livros livros) {
-        System.out.println("/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/");
-        System.out.println("--------------- INFORMAÇÕES DO LIVRO ---------------");
-        System.out.println("Título do livro: " + livros.titulo);
-        System.out.println("Autor do livro: " + livros.autor);
-        System.out.println("Gênero do livro: " + livros.genero);
-        System.out.println("ISBN do livro: " + livros.isbn);
         System.out.println("---------------------------------------------------");
-        System.out.println("/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/");
+        System.out.println("                  INFORMAÇÕES DO LIVRO                ");
+        System.out.println("Título do livro: " + livros.getTitulo());
+        System.out.println("Autor do livro: " + livros.getAutor());
+        System.out.println("Gênero do livro: " + livros.getGenero());
+        System.out.println("ISBN do livro: " + livros.getIsbn());
+        System.out.println("---------------------------------------------------");
     }
+
 }
