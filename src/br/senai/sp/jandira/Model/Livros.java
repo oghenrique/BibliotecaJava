c3d1a63fcbb0d5b1ce75530830b60b14e188f63d
Gustavo Henrique <123705623+oghenrique@users.noreply.github.com>
2023-09-17 01:02:55 -0300

    livros concluido

--- src/br/senai/sp/jandira/Model/Livros.java
+++ src/br/senai/sp/jandira/Model/Livros.java
@@ -1,9 +1,45 @@
 package br.senai.sp.jandira.Model;
 
 public class Livros {
-    String titulo;
-    String isbn;
-    String autor;
-    String genero;
+    String titulo, autor, genero, isbn;
+    boolean emprestado;
 
+    public Livros(String titulo, String autor, String genero, String isbn) {
+        this.titulo = titulo;
+        this.isbn = isbn;
+        this.autor = autor;
+        this.genero = genero;
+        this.emprestado = false;
+    }
+
+    public Livros() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public boolean isEmprestado() {
+        return emprestado;
+    }
+
+    public void emprestar() {
+        emprestado = true;
+    }
+
+    public void devolver() {
+        emprestado = false;
+    }
 }
