344a527b6aed9e73ac997fb7b0e3cbed21325fd3
Gustavo Henrique <123705623+oghenrique@users.noreply.github.com>
2023-09-17 01:04:31 -0300

    cadastro completo

--- src/br/senai/sp/jandira/Model/Register.java
+++ src/br/senai/sp/jandira/Model/Register.java
@@ -3,87 +3,54 @@ package br.senai.sp.jandira.Model;
 import java.util.Scanner;
 
 public class Register {
-    int validaPrint = 0;
-    Scanner teclado = new Scanner(System.in);
-    Usuario usuario = new Usuario();
-    Livros livro = new Livros();
-    Output output = new Output();
+    private int validaPrint = 0;
+    private Scanner teclado = new Scanner(System.in);
 
-    public void BothRegister() {
-        validaPrint = 1;
-        LeitorRegister();
-        LivroRegister();
-        output.PrintUsuario(usuario);
-        output.PrintLivro(livro);
+    public Register() {
     }
 
-    public Usuario LeitorRegister() {
-        System.out.println("---------------- Cadastro Usuário ----------------");
+    public Usuario cadastrarLeitor() {
+        Usuario usuario = new Usuario();
+
+        System.out.println("---------------- Cadastro Leitor -----------------");
         System.out.print("Insira seu nome: ");
-        usuario.nome = teclado.next();
+        usuario.nome = teclado.nextLine();
         System.out.print("Insira seu email: ");
-        usuario.email = teclado.next();
+        usuario.email = teclado.nextLine();
         System.out.print("Insira seu endereço: ");
-        usuario.endereco = teclado.next();
+        usuario.endereco = teclado.nextLine();
+        System.out.print("Insira seu telefone: ");
+        usuario.telefone = teclado.nextLine();
         System.out.println("--------- Usuário Cadastrado com Sucesso ---------");
 
         if (validaPrint != 1) {
+            Output output = new Output();
             output.PrintUsuario(usuario);
         }
 
         return usuario;
     }
 
-    public Livros LivroRegister() {
+    public Livros cadastrarLivro() {
+        Livros livro = new Livros();
+
         System.out.println("---------------- Cadastro Livro ----------------");
         System.out.print("Qual é o título do livro: ");
-        livro.titulo = teclado.next();
-        teclado.next();
+        livro.titulo = teclado.nextLine();
         System.out.print("Qual o autor do livro: ");
-        livro.autor = teclado.next();
-        teclado.next();
+        livro.autor = teclado.nextLine();
         System.out.print("Qual o gênero do livro: ");
-        livro.genero = teclado.next();
-        teclado.next();
+        livro.genero = teclado.nextLine();
         System.out.print("Qual o ISBN do livro: ");
-        livro.isbn = teclado.next();
+        livro.isbn = teclado.nextLine();
         System.out.println("--------- Livro Cadastrado com Sucesso ---------");
 
         if (validaPrint != 1) {
+            Output output = new Output();
             output.PrintLivro(livro);
+
         }
 
         return livro;
-    }
-
-    public void Decision() {
-        String decision;
-
-        System.out.println("Seja Bem Vindo !!!");
-        while (true) {
-            System.out.println("Deseja Cadastrar [Leitor | Livro | Ambos]: ");
-            decision = teclado.next().toLowerCase();
-
-            switch (decision) {
-                case "leitor":
-                    LeitorRegister();
-                    return;
-                case "livro":
-                    LivroRegister();
-                    return;
-                case "ambos":
-                    BothRegister();
-                    return;
-                default:
-                    System.out.println("Opção inválida. Tente novamente.");
-            }
-
-            System.out.println("Deseja Retornar ao Cadastro [1- Sim  2- Não]: ");
-            int continuar = teclado.nextInt();
-
-            if (continuar == 1) {
-                Decision();
-            }
-        }
     }
 }
