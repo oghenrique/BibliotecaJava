6ccd6a2f8a954893d7cc113af5c894bc32b0b34c
Gustavo Henrique <123705623+oghenrique@users.noreply.github.com>
2023-09-17 01:02:18 -0300

    emprestimo concluido

--- src/br/senai/sp/jandira/Model/Emprestimo.java
+++ src/br/senai/sp/jandira/Model/Emprestimo.java
@@ -1,6 +1,25 @@
 package br.senai.sp.jandira.Model;
 
 public class Emprestimo {
-    String dataEmprestimo;
-    String dataDevolucao;
+
+    public boolean emprestarLivro(Catalogo catalogo, String tituloLivro) {
+        Livros livroParaEmprestimo = catalogo.tituloDoLivro(tituloLivro);
+
+        if (livroParaEmprestimo != null && !livroParaEmprestimo.isEmprestado()) {
+            catalogo.emprestarLivro(livroParaEmprestimo);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean devolverLivro(Catalogo catalogo, String tituloLivro) {
+        Livros livroDevolvido = catalogo.tituloDoLivro(tituloLivro);
+
+        if (livroDevolvido != null && livroDevolvido.isEmprestado()) {
+            catalogo.devolverLivro(livroDevolvido);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
